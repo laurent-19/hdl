69c6064b97a547a5383b1530ba4ed0e187c12a72
LIacob106 <liviu.iacob@analog.com>
2022-07-12 13:46:40 +0100

    update makefiles too

--- scripts/adi_env.tcl
+++ scripts/adi_env.tcl
@@ -1,10 +1,10 @@
 
 # environment related stuff
 
-set ad_hdl_dir [file normalize [file join [file dirname [info script]] "../.."]]
+set ad_hdl_dir [file normalize [file join [file dirname [info script]] "../"]]
 
 # assuming that ghdl is cloned next to hdl repository
-set ad_ghdl_dir [file normalize [file join [file dirname [info script]] "../../../ghdl"]]
+set ad_ghdl_dir [file normalize [file join [file dirname [info script]] "../../ghdl"]]
 
 
 if [info exists ::env(ADI_HDL_DIR)] {
