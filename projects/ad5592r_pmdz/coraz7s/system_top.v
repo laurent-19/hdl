ec456ee1f58dba1398569e52a1a212959d4d72df
Maxim Bianca <bmaxim55@yahoo.com>
2022-08-02 16:27:32 +0300

    ad5592r_pmdz: Am modificat fisierul system_top

--- projects/ad5592r_pmdz/coraz7s/system_top.v
+++ projects/ad5592r_pmdz/coraz7s/system_top.v
@@ -65,7 +65,16 @@ module system_top (
   inout   [5:0]   led,
 
   // ad5592r SPI configuration interface
-);
+  //
+   input          spi_mosi,
+  output          spi_miso,
+  output          spi_sclk,
+  output          spi_cs,
+  output          m2k_mosi,
+  output          m2k_miso,
+  output          m2k_sclk,
+  output          m2k_cs
+  );
 
   // internal signals
 
@@ -92,6 +101,12 @@ module system_top (
     .dio_p(led));
 
   assign gpio_i[63:33] = gpio_o[63:33];
+ 
+  assign m2k_sclk = spi_sclk ;
+  assign m2k_mosi  = spi_mosi  ;
+  assign m2k_miso  = spi_miso  ;
+  assign m2k_cs   = spi_cs   ;	
+
   system_wrapper i_system_wrapper (
     .ddr_addr (ddr_addr),
     .ddr_ba (ddr_ba),
@@ -116,15 +131,15 @@ module system_top (
     .fixed_io_ps_srstb (fixed_io_ps_srstb),
     .gpio_i (gpio_i),
     .gpio_o (gpio_o),
-    .spi0_clk_i (),
-    .spi0_clk_o (),
-    .spi0_csn_0_o (),
+    .spi0_clk_i (spi_sclk),
+    .spi0_clk_o (spi_sclk),
+    .spi0_csn_0_o (spi_cs),
     .spi0_csn_1_o (),
     .spi0_csn_2_o (),
     .spi0_csn_i (1'b1),
-    .spi0_sdi_i (),
-    .spi0_sdo_i (),
-    .spi0_sdo_o (),
+    .spi0_sdi_i (spi_mosi),
+    .spi0_sdo_i (spi_miso),
+    .spi0_sdo_o (spi_miso),
     .spi1_clk_i (1'b0),
     .spi1_clk_o (),
     .spi1_csn_0_o (),
